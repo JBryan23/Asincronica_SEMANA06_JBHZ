c27628457a9d9fabd21eabcc252d609a75e55489
Josue Bryan Hernandez Zelaya <124007402+JBryan23@users.noreply.github.com>
2023-09-06 21:56:07 -0600

    Add files via upload

---
+++ SEMANA 06 EJERCICIOS LINQ JBHZ/Ejercicio1_LINQ_JosueBryanHernandezZela/Ejercicio1_LINQ_JosueBryanHernandezZela/Program.cs
@@ -0,0 +1,20 @@
+﻿//La operacion completa incluye crear un origen de datos, definir la expresion de consulta
+//y ejecutar la consulta en una instruccion foreach
+
+class where_ejercicio1
+{
+    static void Main()
+    {
+        int[] scores = { 97, 92, 81, 60 };
+        var consultas_De_Numeros =
+            from numero in scores
+            where numero > 80
+            select numero;
+       
+        foreach (var num in consultas_De_Numeros)
+        {   
+            Console.WriteLine("Su resultado es:" + num.ToString());
+        }
+    }
+}
+// Josue Bryan Hernandez Zelaya
---
+++ SEMANA 06 EJERCICIOS LINQ JBHZ/Ejercicio2_LINQ_JosueBryanHernandezZelay/Ejercicio2_LINQ_JosueBryanHernandezZelay/Program.cs
@@ -0,0 +1,23 @@
+﻿//La operacion completa incluye crear un origen de datos, definir la expresion de consulta
+//y ejecutar la consulta en una instruccion foreach
+class where_ejercicio2
+{
+    static void Main()
+    {
+        int[] scores = { 90, 71, 82, 93, 75, 82 };
+        var consultas_De_Numeros =
+            from numero in scores
+            where numero > 80
+            orderby numero descending
+            select numero;
+
+       
+        foreach (var num2 in consultas_De_Numeros)  
+        {
+           
+
+            Console.WriteLine("Su resultado es:" + num2.ToString());
+        }
+    }
+}
+// Josue Bryan Hernandez Zelaya
